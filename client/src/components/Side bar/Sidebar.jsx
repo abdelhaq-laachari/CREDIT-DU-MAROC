ac903414980fa1605b7e4e1e8fb514289b35c7ba
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2023-01-09 18:47:52 +0100

    feat: side bar done

--- client/src/components/Side bar/Sidebar.jsx
+++ client/src/components/Side bar/Sidebar.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { AiFillCreditCard, AiOutlineLogout, AiOutlineTransaction } from "react-icons/ai";
-import {MdOutlinePayments} from "react-icons/md";
+import {
+  AiFillCreditCard,
+  AiOutlineLogout,
+  AiOutlineTransaction,
+} from "react-icons/ai";
+import { MdOutlinePayments } from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
 const App = () => {
   const [open, setOpen] = useState(true);
@@ -10,27 +14,24 @@ const App = () => {
     <div className="flex">
       <div
         className={` ${
-          open ? "w-72" : "w-20 "
-        } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
+          open ? "w-64" : "w-20 "
+        } h-screen p-5  pt-8 relative duration-300`}
       >
         <img
           src={require("../../assets/icons/control.png")}
           alt="control"
-          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+          className={`absolute cursor-pointer -right-3 top-9 w-7
            border-2 rounded-full  ${!open && "rotate-180"}`}
           onClick={() => setOpen(!open)}
         />
         <div className="flex gap-x-4 items-center">
           <img
-            src={require("../../assets/icons/logo.png")}
+            src="https://seeklogo.com/images/C/credit-du-maroc-logo-80D6D85982-seeklogo.com.png"
             alt="logo"
-            className={`cursor-pointer duration-500 ${
-              open && "rotate-[360deg]"
-            }`}
-          />
+            className="cursor-pointer duration-500 w-12"/>
           <h1
-            className={`text-white origin-left font-medium text-xl duration-200 ${
-              !open && "scale-0"
+            className={`origin-left font-medium text-xl duration-200 ${
+              !open && "hidden"
             }`}
           >
             Credit du maroc
@@ -38,11 +39,7 @@ const App = () => {
         </div>
         <ul className="pt-9">
           <Link to="/search">
-            <li
-              className={`flex mb-2 rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-md items-center gap-x-4
-                "mt-2" "bg-light-white"
-                 `}
-            >
+            <li className="flex mb-2 rounded-md p-2 cursor-pointer hover:bg-gray-600 hover:text-white transition-colors text-md items-center gap-x-4">
               <AiOutlineTransaction />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 Transaction
@@ -50,11 +47,7 @@ const App = () => {
             </li>
           </Link>
           <Link to="/search">
-            <li
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4
-                "mt-2" "bg-light-white"
-                 `}
-            >
+            <li className="flex mb-2 rounded-md p-2 cursor-pointer hover:bg-gray-600 hover:text-white transition-colors text-md items-center gap-x-4">
               <AiFillCreditCard />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 Cards
@@ -62,11 +55,7 @@ const App = () => {
             </li>
           </Link>
           <Link to="/search">
-            <li
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4
-                "mt-2" "bg-light-white"
-                 `}
-            >
+            <li className="flex mb-2 rounded-md p-2 cursor-pointer hover:bg-gray-600 hover:text-white transition-colors text-md items-center gap-x-4">
               <MdOutlinePayments />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 Payments
@@ -74,11 +63,7 @@ const App = () => {
             </li>
           </Link>
           <Link to="/search">
-            <li
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4
-                "mt-2" "bg-light-white"
-                 `}
-            >
+            <li className="flex mb-2 rounded-md p-2 cursor-pointer hover:bg-gray-600 hover:text-white transition-colors text-md items-center gap-x-4">
               <FaUserCircle />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 Profile
@@ -86,11 +71,7 @@ const App = () => {
             </li>
           </Link>
           <Link to="/search">
-            <li
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4
-                "mt-2" "bg-light-white"
-                 `}
-            >
+            <li className="flex mb-2 rounded-md p-2 cursor-pointer hover:bg-gray-600 hover:text-white transition-colors text-md items-center gap-x-4">
               <AiOutlineLogout />
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 Log Out
